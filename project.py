e594989e4ae76ac1fd0d466bde3a4350590369ab
Varun B <122134747+Varunbtech9682@users.noreply.github.com>
2024-03-20 14:13:24 +0530

    Updated project.py

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+import re
 import json
 
 def get_latest_stories():
@@ -7,14 +7,16 @@ def get_latest_stories():
     response = requests.get(url)
 
     if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        featured_voices_list = soup.find('ul', class_='featured-voices__list')
-        h3_texts = [h3.get_text(strip=True) for h3 in featured_voices_list.find_all('h3')]
-        first_anchor_links = [li.a['href'] for li in featured_voices_list.find_all('li')]
+        html_content = response.text
+        title_pattern = r'<h3 class="featured-voices__list-item-headline display-block">(.*?)</h3>'
+        url_pattern = r'<a href="(.*?)">'
+
+        titles = re.findall(title_pattern, html_content)
+        urls = re.findall(url_pattern, html_content)
 
         stories = []
-        for title, url in zip(h3_texts, first_anchor_links):
-            stories.append({'title': title, 'url': url})
+        for title, url in zip(titles, urls):
+            stories.append({'title': title.strip(), 'url': url})
 
         return json.dumps(stories, indent=4)
     else:
@@ -25,6 +27,8 @@ def get_latest_stories():
 def get_time_stories():
     latest_stories = get_latest_stories()
     return latest_stories
+
 print(get_time_stories())
+
 
 
